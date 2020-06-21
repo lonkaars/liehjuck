75e40b905a045d02ff977176c92f5dace4231ef0
Loek Le Blansch <l.leblansch@gmail.com>
2020-06-21 14:42:23 +0200

    start using cmake

--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 # PWS Engine
 
 Dit is de repository voor de engine
+
+## Bouwen
+
+```
+mkdir build
+cmake ../src
+cmake --build .
+./pws-engine
+```
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+#include <X11/Xlib.h>
+#include <X11/Xos.h>
+#include <X11/Xutil.h>
+
+int main()
+{
+    std::cout << "test\n";
+    return 0;
+}
