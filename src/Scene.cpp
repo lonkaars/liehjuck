5c7d775094240bbe78f46827ba5a5ec301a55b19
amieleg <chickencoreore@gmail.com>
2020-07-09 15:48:26 +0200

    Orientation implemented besides getter and setters

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1,8 +1,38 @@
 #include "Scene.h"
 
+#include <math.h>
+
+// For position class
 Position::Position(float X, float Y, float Z)
 {
     x = X;
     y = y;
     z = Z;
+}
+
+
+// For orientation class
+float Orientation::toDegrees(float degrees)
+{
+    float result = fmod(degrees, 360);
+    return result;
+}
+
+Orientation::Orientation(float X, float Y, float Z)
+{
+    x = toDegrees(x);
+    y = toDegrees(y);
+    z = toDegrees(z);
+}
+
+// For color class
+
+// In case of a color being bigger than 255 or smaller than 0, it should either:
+// 1. Throw an exception
+// 2. Default to a value of 0, which will result in a black color
+Color::Color(int Red, int Green, int Blue)
+{
+    red = Red;
+    green = Green;
+    blue = Blue;
 }
--- src/Scene.h
+++ src/Scene.h
@@ -2,7 +2,10 @@
 #define SCENE_H
 
 #include <vector>
+#include <exception>
 
+// Position does not contain any significant behavior, thus making
+// It sensible for x, y and z to be public
 struct Position
 {
     float x;
@@ -12,14 +15,46 @@ struct Position
     Position(float X, float Y, float Z);
 };
 
+// Orientation does contains some behavior, mainly that an orientation
+// can only be between 0 and 360
 class Orientation
 {
+    float x;
+    float y;
+    float z;
+
+    public:
+        static float toDegrees(float degrees);
+
+        Orientation(float X, float Y, float Z);
+
+        // I'm not sure if it makes sense for these to be defined in 
+        // the header or the normal file
+        float getX();
+        float getY();
+        float getZ();
+        void setX();
+        void setY();
+        void setZ();
 
 };
 
+// Each color must be between 0 and 255
 class Color
 {
+    int red;
+    int green;
+    int blue;
 
+    public:
+        Color(int Red, int Green, int Blue);
+
+        float getX();
+        float getY();
+        float getZ();
+        void setX();
+        void setY();
+        void setZ();
 };
 
 class Camera
