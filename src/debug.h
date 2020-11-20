3e1ff2b152e897d69cf77ac9f2429392e8404196
lonkaars <l.leblansch@gmail.com>
2020-11-20 21:47:48 +0100

    clang-format

--- src/debug.h
+++ src/debug.h
@@ -2,9 +2,10 @@
 #include "scene.h"
 #include "win.h"
 
-namespace debug {
+namespace debug
+{
 
-void line(jdscn::Position2D, jdscn::Position2D, jdscn::Color, win::Canvas&);
-void draw_debug_axes(jdscn::Camera, win::Canvas&);
+void line(jdscn::Position2D, jdscn::Position2D, jdscn::Color, win::Canvas &);
+void draw_debug_axes(jdscn::Camera, win::Canvas &);
 
-}
+} // namespace debug
